b8c4751e2643e9ce9c966e8b6461fa21abe1beff
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2025-08-08 21:39:26 -0500

    interface completa

--- lib/pages/deportes_favoritos_page.dart
+++ lib/pages/deportes_favoritos_page.dart
@@ -14,8 +14,18 @@ class DeportesFavoritosPage extends StatelessWidget {
     );
   }
 
+  Widget buildTitle(String title) {
+    return Text(
+      title,
+      style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeigth = MediaQuery.of(context).size.height;
+
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -29,17 +39,53 @@ class DeportesFavoritosPage extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                "¿Cuáles son tus deportes favoritos?",
-                style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+              buildTitle("¿Cuáles son tus deportes favoritos?"),
+              SizedBox(height: 16),
+              Center(
+                child: Wrap(
+                  spacing: 8, //espaciado horizontal entre elementos
+                  runSpacing: 8, //espaciado vertical entre elementos
+                  alignment: WrapAlignment.spaceAround,
+                  children: [
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Crossfit"),
+                    buildDeportButton("Baloncesto"),
+                  ],
+                ),
               ),
-              Row(
-                children: [
-                  buildDeportButton("Gimnasio"),
-                  buildDeportButton("Crossfit"),
-                  buildDeportButton("Baloncesto"),
-                ],
+              Divider(height: 40, thickness: 4),
+              buildTitle("Mis deportes favoritos son"),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.all(16),
+                width: screenWidth,
+                height: screenHeigth / 2,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(25),
+                  border: Border.all(color: Colors.black, width: 2),
+                ),
+                child: Wrap(
+                  spacing: 8, //espaciado horizontal entre elementos
+                  runSpacing: 8, //espaciado vertical entre elementos
+                  alignment: WrapAlignment.spaceAround,
+                  children: [
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Gimnasio"),
+                    buildDeportButton("Crossfit"),
+                    buildDeportButton("Baloncesto"),
+                  ],
+                ),
               ),
             ],
           ),
